e0b8445ef2ab257a925f5aef6dac0dc1ad6e9636
Ajith Saju <ajith.saju@qburst.com>
2024-04-19 17:03:38 +0530

    stripe integration

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,43 @@
+dmx.config({
+  "BlogWriting": {
+    "repeat1": {
+      "meta": [
+        {
+          "name": "id",
+          "type": "number"
+        },
+        {
+          "name": "first_name",
+          "type": "text"
+        },
+        {
+          "name": "last_name",
+          "type": "text"
+        },
+        {
+          "name": "email",
+          "type": "text"
+        },
+        {
+          "name": "gender",
+          "type": "text"
+        }
+      ],
+      "outputType": "object"
+    },
+    "datastore1": [
+      {
+        "type": "text",
+        "name": "price"
+      }
+    ]
+  },
+  "SeoStrategy": {
+    "datastore1": [
+      {
+        "type": "text",
+        "name": "priceId"
+      }
+    ]
+  }
+});
